e2cfba1489630caabfb339ee3e41188ba33c21cc
Matheus-Vict0r <127435388+Matheus-Vict0r@users.noreply.github.com>
2023-09-14 18:17:39 -0300

    indo pro Git

---
+++ AppTeste-main/src/main/java/com/example/appteste/telatres.java
@@ -0,0 +1,14 @@
+package com.example.appteste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telatres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telatres);
+    }
+}
